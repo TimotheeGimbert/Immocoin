4b19c5ee14afdf30d6cc87056d559a1c4147854c
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-30 19:24:35 +0200

    changes api again

--- pages/api/axiosMethods.js
+++ pages/api/axiosMethods.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 
-// const APIBaseUrl = 'https://api-immocoin.herokuapp.com'
-const APIBaseUrl = 'https://immocoin-api-51.herokuapp.com/'
+const APIBaseUrl = 'https://api-immocoin.herokuapp.com'
+//const APIBaseUrl = 'https://immocoin-api-51.herokuapp.com/'
 const APIRequest = axios.create({ baseURL: APIBaseUrl });
 
 APIRequest.interceptors.request.use(({ headers, ...config }) => ({
